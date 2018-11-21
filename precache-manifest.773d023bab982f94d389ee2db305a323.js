0f24bd0458abe3b17e261634d65402650af82f95
Brennan M <brennanmcvicar@gmail.com>
2018-11-20 23:12:16 -0500

    Updates

---
+++ precache-manifest.773d023bab982f94d389ee2db305a323.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eacb0c7d3fcb360d472",
+    "url": "/react-player-issue-520/static/js/runtime~main.2eacb0c7.js"
+  },
+  {
+    "revision": "e9a95d45c4c4c8f81afc",
+    "url": "/react-player-issue-520/static/js/main.e9a95d45.chunk.js"
+  },
+  {
+    "revision": "2f1d40ea5bc79a689a61",
+    "url": "/react-player-issue-520/static/js/1.2f1d40ea.chunk.js"
+  },
+  {
+    "revision": "b788fd727d054697d8e00d98c10b29f4",
+    "url": "/react-player-issue-520/index.html"
+  }
+];
