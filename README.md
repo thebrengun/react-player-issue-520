6a1a2725f54c81574a322ac53f506b7e65a6e2e7
Brennan M <brennanmcvicar@gmail.com>
2018-11-20 23:04:35 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,44 +1,29 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Demonstrates an issue where certain videos will cause the React Player to stop controlling the video playback. When the second video begins playing, the play/pause button will cease to control the video. In Safari it continues to work though the second video does not seem to autoplay, despite the first video autoplaying just fine.
 
-## Available Scripts
+[Demo](https://thebrengun.github.io/react-player-issue-520/)
 
-In the project directory, you can run:
+##Chrome:
+###Version 70.0.3538.110
 
-### `npm start`
+- 1st Video Loads and autoplays if playing is true as expected.
+- 2nd Video Loads and autoplays but without firing onPlaying event.
+- Video can no longer be controlled by playing prop.
+- Interacting with the Vimeo player regains expected functionality.
+- Logged to console: nothing
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+##Firefox
+###Version 63.0.3
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+- 1st Video Loads and autoplays if playing is true as expected.
+- 2nd Video Loads and autoplays but without firing onPlaying event.
+- Video can no longer be controlled by playing prop.
+- Interacting with the Vimeo player regains expected functionality.
+- Logged to console: InvalidStateError: An attempt was made to use an object that is not, or is no longer, usable - player.js:2
 
-### `npm test`
+##Safari
+###Version 12.0.1
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+- 1st Video Loads and autoplays if playing is true as expected.
+- 2nd Video Loads but does not autoplay even if playing is true.
+- Video can be controlled by playing prop.
+- Logged to console: Successfuly preconnected to https://f.vimeocdn.com/
--- src/index.js
+++ src/index.js
@@ -1,12 +1,166 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactPlayer from 'react-player';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.PureComponent {
+	constructor() {
+		super();
+		this.state = {
+			active: 0,
+			allowPlaying: true,
+			videoIsPlaying: false,
+			videos: [
+				{
+					title: 'Restricted Embed 1', 
+					vimeoId: '301926039'
+				},
+		    {
+		      title: "Restricted Embed 2",
+		      vimeoId: "85876630",
+		    },
+		    {
+					title: 'Restricted Embed 3', 
+					vimeoId: '301925886'
+				},
+				{
+					title: 'Embeddable Anywhere 1', 
+					vimeoId: '301926457'
+				},
+				{
+					title: 'Embeddable Anywhere 2', 
+					vimeoId: '301926243'
+				}
+			]
+		};
+		
+		this.handlePlay = this.handlePlay.bind(this);
+		this.handlePause = this.handlePause.bind(this);
+		this.handleOnEnded = this.handleOnEnded.bind(this);
+		this.handleToggleAllowPlaying = this.handleToggleAllowPlaying.bind(this);
+		this.handleSelectVideo = this.handleSelectVideo.bind(this);
+	}
+	
+	handlePlay() {
+		this.setState({videoIsPlaying: true});
+	}
+	
+	handlePause() {
+		this.setState({videoIsPlaying: false});
+	}
+	
+	handleToggleAllowPlaying(e) {
+		this.setState(prevState => ({allowPlaying: !prevState.allowPlaying}));
+	}
+	
+	handleOnEnded() {
+		this.setState(state => ({
+			active: state.active + 1 < state.videos.length ? state.active + 1 : 0
+		}));
+	}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	handleSelectVideo(i) {
+		this.setState({active: i});
+	}
+	
+	render() {
+		const { title, vimeoId } = this.state.videos[this.state.active];
+		return (
+			<div>
+				<h1>React Player <a href="https://github.com/CookPete/react-player/issues/520" target="_blank" rel="noopener noreferrer">Issue #520</a></h1>
+				<p>Demonstrates an issue where certain videos will cause the React Player to stop controlling the video playback. When the second video begins playing, the play/pause button will cease to control the video. In Safari it continues to work though the second video does not seem to autoplay, despite the first video autoplaying just fine.</p>
+
+				<div className="row">
+					<div className="col">
+						<h2>{title}</h2>
+						<p>
+							<button onClick={this.handleToggleAllowPlaying}>
+								{this.state.allowPlaying ? 'Pause' : 'Play'}
+							</button>
+						</p>
+						<div className="player-wrapper">
+							<ReactPlayer 
+								playing={this.state.allowPlaying} 
+								url={'https://www.vimeo.com/' + vimeoId} 
+								onStart={this.handlePlay}
+								onPlay={this.handlePlay} 
+								onPause={this.handlePause} 
+								onEnded={this.handleOnEnded} 
+								className="react-player"
+							/>
+						</div>
+						<p>
+							{this.state.allowPlaying ? 
+								'Playing Allowed' : 'Playing Disallowed'
+							}
+						</p>
+						<p>
+							{this.state.videoIsPlaying ? 
+								'Video Is Playing' : 'Video Is Not Playing'
+							}
+						</p>
+						<ul>
+							{this.state.videos.map(
+								({title}, i) => {
+									const activeVideo = i === this.state.active;
+									return (
+										<li 
+											key={title} 
+											style={{
+												cursor: 'pointer', 
+												color:  activeVideo ? '#ff0000' : '#000000'
+											}} 
+
+											onClick={(e) => {
+												if(!activeVideo) {
+													this.handleSelectVideo(i);
+												}
+											}}
+										>
+											{title}
+										</li>
+									);
+								}
+							)}
+						</ul>
+					</div>
+					<div className="col">
+						<h2>Browser Observations</h2>
+						<h3>Chrome:</h3>
+						<strong>Version 70.0.3538.110</strong>
+						<ul>
+							<li>1st Video Loads and autoplays if playing is true as expected.</li>
+							<li>2nd Video Loads and autoplays but without firing onPlaying event.</li>
+							<li>Video can no longer be controlled by playing prop.</li>
+							<li>Interacting with the Vimeo player regains expected functionality.</li>
+							<li>Logged to console: nothing</li>
+						</ul>
+
+						<h3>Firefox</h3>
+						<strong>Version 63.0.3</strong>
+						<ul>
+							<li>1st Video Loads and autoplays if playing is true as expected.</li>
+							<li>2nd Video Loads and autoplays but without firing onPlaying event.</li>
+							<li>Video can no longer be controlled by playing prop.</li>
+							<li>Interacting with the Vimeo player regains expected functionality.</li>
+							<li>Logged to console: InvalidStateError: An attempt was made to use an object that is not, or is no longer, usable - player.js:2</li>
+						</ul>
+
+						<h3>Safari</h3>
+						<strong>Version 12.0.1</strong>
+						<ul>
+							<li>1st Video Loads and autoplays if playing is true as expected.</li>
+							<li>2nd Video Loads but does not autoplay even if playing is true.</li>
+							<li>Video can be controlled by playing prop.</li>
+							<li>Logged to console: Successfuly preconnected to https://f.vimeocdn.com/</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('player-demo')
+);
